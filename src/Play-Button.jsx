d7d5a98a7eac6682ad13886481d2280f2fbd0ce6
Daniel Mocan <contact_daniel@yahoo.com>
2017-08-29 14:13:38 +0300

    Changed Play Button from react create class to pure component

--- src/Play-Button.jsx
+++ src/Play-Button.jsx
@@ -1,18 +1,10 @@
 /* eslint-disable max-len */
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default React.createClass({
-  displayName: 'PlayButton',
-
-  propTypes: {
-    onClick: React.PropTypes.func
-  },
-
-  render() {
-    return (
-      <button
+const PlayButton = ({ onClick } ) => ( <button
         className='vimeo-play-button'
-        onClick={ this.props.onClick }
+        onClick={ onClick }
         type='button'>
         <svg
           version='1.1'
@@ -20,7 +12,10 @@ export default React.createClass({
           xmlns='http://www.w3.org/2000/svg'>
           <path d='M79.674,53.719c2.59-2.046,2.59-5.392,0-7.437L22.566,1.053C19.977-0.993,18,0.035,18,3.335v93.331c0,3.3,1.977,4.326,4.566,2.281L79.674,53.719z' />
         </svg>
-      </button>
-    );
-  }
-});
+      </button> );
+
+PlayButton.PropTypes = {
+  onClick: PropTypes.func
+};
+
+export default PlayButton;
